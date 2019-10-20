43e3778aa8ac9aa5507399d3f351a01fa61cd669
usynin.yury <usynin.yury@yandex.ru>
2019-10-20 22:47:00 +0300

    ch_6

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -12,6 +12,9 @@ namespace Test_1
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Hello World!"); Console.WriteLine("Hello World!");
         }
     }
 }
