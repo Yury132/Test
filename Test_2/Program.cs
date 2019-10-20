fe41a6df76f32cc152aa3217c606bbd48f05a5c2
usynin.yury <usynin.yury@yandex.ru>
2019-10-20 23:20:51 +0300

    n2

--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -7,6 +7,7 @@ namespace Test_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wwwworld!");
+            Console.WriteLine("Hello Wwwworld!");
         }
     }
 }
